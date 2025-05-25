6b4bed2d9e95f09dfe4c62c0edecedb5a578cd17
bijoymalaker <bijoymalaker7@gmail.com>
2025-05-25 22:46:30 +0600

    login and register page completed

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,66 +1,182 @@
 <template>
-    <div class="container">
-        <form action="/login" method="POST" class="text-center border border-light p-5">
-            <p class="h4 mb-4">Sign in</p>
-            <!-- Email input -->
-            <div data-mdb-input-init class="form-outline mb-4">
-                <input type="email" id="form2Example1" class="form-control" required />
-                <label class="form-label" for="form2Example1">Email address</label>
-            </div>
+  <div class="container">
+    <form
+      @submit.prevent="handleSubmit"
+      class="text-center border border-light p-5"
+      novalidate
+    >
+      <p class="h4 mb-4">Sign in</p>
+      <!-- Email input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input
+          type="email"
+          id="form2Example1"
+          class="form-control"
+          v-model="email"
+          :class="{ 'is-invalid': emailError }"
+          required
+        />
+        <label class="form-label" for="form2Example1">Email address</label>
+        <div v-if="emailError" class="invalid-feedback">{{ emailError }}</div>
+      </div>
 
-            <!-- Password input -->
-            <div data-mdb-input-init class="form-outline mb-4">
-                <input type="password" id="form2Example2" class="form-control" required />
-                <label class="form-label" for="form2Example2">Password</label>
-            </div>
+      <!-- Password input -->
+      <div data-mdb-input-init class="form-outline mb-4">
+        <input
+          type="password"
+          id="form2Example2"
+          class="form-control"
+          v-model="password"
+          :class="{ 'is-invalid': passwordError }"
+          required
+        />
+        <label class="form-label" for="form2Example2">Password</label>
+        <div v-if="passwordError" class="invalid-feedback">
+          {{ passwordError }}
+        </div>
+      </div>
 
-            <!-- 2 column grid layout for inline styling -->
-            <div class="row mb-4">
-                <div class="col d-flex justify-content-center">
-                    <!-- Checkbox -->
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                        <label class="form-check-label" for="form2Example31"> Remember me </label>
-                    </div>
-                </div>
+      <!-- 2 column grid layout for inline styling -->
+      <div class="row mb-4">
+        <div class="col d-flex justify-content-center">
+          <!-- Checkbox -->
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              v-model="remember"
+              id="form2Example31"
+            />
+            <label class="form-check-label" for="form2Example31">
+              Remember me
+            </label>
+          </div>
+        </div>
 
-                <div class="col">
-                    <!-- Simple link -->
-                    <a href="#!">Forgot password?</a>
-                </div>
-            </div>
+        <div class="col">
+          <!-- Simple link -->
+          <a href="#!">Forgot password?</a>
+        </div>
+      </div>
 
-            <!-- Submit button -->
-            <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign
-                in</button>
+      <!-- Submit button -->
+      <button
+        type="submit"
+        data-mdb-button-init
+        data-mdb-ripple-init
+        class="btn btn-primary btn-block mb-4"
+      >
+        Sign in
+      </button>
 
-            <!-- Register buttons -->
-            <div class="text-center">
-                <p>Not a member? <router-link to="/register">Register</router-link></p>
-                <p>or sign up with:</p>
-                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-facebook-f"></i>
-                </button>
+      <!-- Register buttons -->
+      <div class="text-center">
+        <p>Not a member? <router-link to="/register">Register</router-link></p>
+        <p>or sign up with:</p>
+        <button
+          type="button"
+          data-mdb-button-init
+          data-mdb-ripple-init
+          class="btn btn-link btn-floating mx-1"
+        >
+          <i class="fab fa-facebook-f"></i>
+        </button>
 
-                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-google"></i>
-                </button>
+        <button
+          type="button"
+          data-mdb-button-init
+          data-mdb-ripple-init
+          class="btn btn-link btn-floating mx-1"
+        >
+          <i class="fab fa-google"></i>
+        </button>
 
-                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-twitter"></i>
-                </button>
+        <button
+          type="button"
+          data-mdb-button-init
+          data-mdb-ripple-init
+          class="btn btn-link btn-floating mx-1"
+        >
+          <i class="fab fa-twitter"></i>
+        </button>
 
-                <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-link btn-floating mx-1">
-                    <i class="fab fa-github"></i>
-                </button>
-            </div>
-        </form>
-
-    </div>
+        <button
+          type="button"
+          data-mdb-button-init
+          data-mdb-ripple-init
+          class="btn btn-link btn-floating mx-1"
+        >
+          <i class="fab fa-github"></i>
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
+
 <script>
 export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      remember: false,
+      emailError: "",
+      passwordError: "",
+    };
+  },
+  methods: {
+    validateEmail(email) {
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(email);
+    },
+    handleSubmit() {
+      this.emailError = "";
+      this.passwordError = "";
 
-}
+      let valid = true;
+
+      if (!this.email) {
+        this.emailError = "Email is required.";
+        valid = false;
+      } else if (!this.validateEmail(this.email)) {
+        this.emailError = "Please enter a valid email address.";
+        valid = false;
+      }
+
+      if (!this.password) {
+        this.passwordError = "Password is required.";
+        valid = false;
+      } else if (this.password.length < 6) {
+        this.passwordError = "Password must be at least 6 characters.";
+        valid = false;
+      }
+
+      if (valid) {
+        // Submit form logic here (e.g., API call)
+        // For now, just log the data
+        // You can replace this with your actual login logic
+        // console.log({
+        //     email: this.email,
+        //     password: this.password,
+        //     remember: this.remember
+        alert(
+          "Login successful!" +
+            JSON.stringify({
+              email: this.email,
+              password: this.password,
+            })
+        );
+      }
+    },
+  },
+};
 </script>
-<style></style>
+<style>
+.is-invalid {
+  border-color: #dc3545;
+}
+.invalid-feedback {
+  color: #dc3545;
+  font-size: 0.875em;
+}
+</style>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,96 +1,221 @@
 <template>
-    <div>
-        <section class="vh-100" style="background-color: #eee;">
-            <div class="container h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-lg-12 col-xl-11">
-                        <div class="card text-black" style="border-radius: 25px;">
-                            <div class="card-body p-md-5">
-                                <div class="row justify-content-center">
-                                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    <section class="vh-100" style="background-color: #eee">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" style="border-radius: 25px">
+              <div class="card-body p-md-5">
+                <div class="row justify-content-center">
+                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      Sign up
+                    </p>
 
-                                        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
-                                        <form class="mx-1 mx-md-4" action="/login" method="POST">
-
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="text" id="form3Example1c" class="form-control" required />
-                                                    <label class="form-label" for="form3Example1c">Your Name</label>
-                                                </div>
-                                            </div>
-
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="email" id="form3Example3c" class="form-control" required />
-                                                    <label class="form-label" for="form3Example3c">Your Email</label>
-                                                </div>
-                                            </div>
-
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="password" id="form3Example4c" class="form-control" required />
-                                                    <label class="form-label" for="form3Example4c">Password</label>
-                                                </div>
-                                            </div>
-
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="password" id="form3Example4cd" class="form-control" required />
-                                                    <label class="form-label" for="form3Example4cd">Repeat your
-                                                        password</label>
-                                                </div>
-                                            </div>
-
-                                            <div class="form-check d-flex justify-content-center mb-5">
-                                                <input class="form-check-input me-2" type="checkbox" value=""
-                                                    id="form2Example3c" required />
-                                                <label class="form-check-label" for="form2Example3">
-                                                    I agree all statements in <a href="#!">Terms of service</a>
-                                                </label>
-                                            </div>
-
-                                            <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                <button type="submit" data-mdb-button-init data-mdb-ripple-init
-                                                    class="btn btn-primary btn-lg">Register</button>
-                                            </div>
-
-                                        </form>
-
-                                    </div>
-                                    <div
-                                        class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                                            class="img-fluid" alt="Sample image">
-
-                                    </div>
-                                </div>
-                            </div>
+                    <form class="mx-1 mx-md-4" @submit.prevent="onSubmit">
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                        <div
+                          data-mdb-input-init
+                          class="form-outline flex-fill mb-0"
+                        >
+                          <input
+                            type="text"
+                            id="form3Example1c"
+                            class="form-control"
+                            v-model="form.name"
+                            :class="{ 'is-invalid': errors.name }"
+                            required
+                          />
+                          <label class="form-label" for="form3Example1c"
+                            >Your Name</label
+                          >
+                          <div class="invalid-feedback" v-if="errors.name">
+                            {{ errors.name }}
+                          </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+                      </div>
 
-    </div>
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                        <div
+                          data-mdb-input-init
+                          class="form-outline flex-fill mb-0"
+                        >
+                          <input
+                            type="email"
+                            id="form3Example3c"
+                            class="form-control"
+                            v-model="form.email"
+                            :class="{ 'is-invalid': errors.email }"
+                            required
+                          />
+                          <label class="form-label" for="form3Example3c"
+                            >Your Email</label
+                          >
+                          <div class="invalid-feedback" v-if="errors.email">
+                            {{ errors.email }}
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <div
+                          data-mdb-input-init
+                          class="form-outline flex-fill mb-0"
+                        >
+                          <input
+                            type="password"
+                            id="form3Example4c"
+                            class="form-control"
+                            v-model="form.password"
+                            :class="{ 'is-invalid': errors.password }"
+                            required
+                          />
+                          <label class="form-label" for="form3Example4c"
+                            >Password</label
+                          >
+                          <div class="invalid-feedback" v-if="errors.password">
+                            {{ errors.password }}
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                        <div
+                          data-mdb-input-init
+                          class="form-outline flex-fill mb-0"
+                        >
+                          <input
+                            type="password"
+                            id="form3Example4cd"
+                            class="form-control"
+                            v-model="form.repeatPassword"
+                            :class="{ 'is-invalid': errors.repeatPassword }"
+                            required
+                          />
+                          <label class="form-label" for="form3Example4cd"
+                            >Repeat your password</label
+                          >
+                          <div
+                            class="invalid-feedback"
+                            v-if="errors.repeatPassword"
+                          >
+                            {{ errors.repeatPassword }}
+                          </div>
+                        </div>
+                      </div>
+
+                      <div
+                        class="form-check d-flex justify-content-center mb-5"
+                      >
+                        <input
+                          class="form-check-input me-2"
+                          type="checkbox"
+                          id="form2Example3c"
+                          v-model="form.terms"
+                          :class="{ 'is-invalid': errors.terms }"
+                          required
+                        />
+                        <label class="form-check-label" for="form2Example3c">
+                          I agree all statements in
+                          <a href="#!">Terms of service</a>
+                        </label>
+                        <div class="invalid-feedback" v-if="errors.terms">
+                          {{ errors.terms }}
+                        </div>
+                      </div>
+
+                      <div
+                        class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                      >
+                        <button
+                          type="submit"
+                          data-mdb-button-init
+                          data-mdb-ripple-init
+                          class="btn btn-primary btn-lg"
+                        >
+                          Register
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div
+                    class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2"
+                  >
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      class="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 </template>
 <script>
 export default {
-    name: 'Register',
-    data() {
-        return {
-            // Add any data properties you need here
-        };
+  name: "Register",
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+        terms: false,
+      },
+      errors: {},
+    };
+  },
+  methods: {
+    validate() {
+      const errors = {};
+      if (!this.form.name) {
+        errors.name = "Name is required.";
+      }
+      if (!this.form.email) {
+        errors.email = "Email is required.";
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.form.email)) {
+        errors.email = "Email is invalid.";
+      }
+      if (!this.form.password) {
+        errors.password = "Password is required.";
+      } else if (this.form.password.length < 6) {
+        errors.password = "Password must be at least 6 characters.";
+      }
+      if (!this.form.repeatPassword) {
+        errors.repeatPassword = "Please repeat your password.";
+      } else if (this.form.password !== this.form.repeatPassword) {
+        errors.repeatPassword = "Passwords do not match.";
+      }
+      if (!this.form.terms) {
+        errors.terms = "You must agree to the terms.";
+      }
+      this.errors = errors;
+      return Object.keys(errors).length === 0;
     },
-    methods: {
-        // Add any methods you need here
-    }
-
-}
+    onSubmit() {
+      if (this.validate()) {
+        // Submit form logic here
+        alert("Registration successful!");
+        // Reset form if needed
+        this.form = {
+          name: "",
+          email: "",
+          password: "",
+          repeatPassword: "",
+          terms: false,
+        };
+        this.errors = {};
+      }
+    },
+  },
+};
 </script>
 <style></style>
